8071cb08e877a0f907fa8407782e7a841786dd4a
Coty Clark <coty.clark@gmail.com>
2019-02-26 22:32:54 -0500

    Section two_4 "Share to: "

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -29,5 +29,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnShareToOtherApps.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to: "))
+
+        }
+
     }
 }
