493d634b4febb29d7b44687aa2f2c60bdff71f03
Coty Clark <coty.clark@gmail.com>
2019-02-26 22:00:31 -0500

    Section two_4

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -21,9 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         btnSendMsgToNextActivity.setOnClickListener {
             val message: String = etUserMessage.text.toString()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/example/msgshareapp/SecondActivity.kt
@@ -3,12 +3,21 @@ package com.example.msgshareapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle: Bundle? = intent.extras
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        txvUserMessage.text = msg
     }
 
 
