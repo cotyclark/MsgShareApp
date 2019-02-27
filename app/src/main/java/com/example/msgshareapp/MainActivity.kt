a0511e9f66bcc361a752641709eb7098987d2215
Coty Clark <coty.clark@gmail.com>
2019-02-26 21:41:46 -0500

    Section two_3

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,10 +16,16 @@ class MainActivity : AppCompatActivity() {
         btnShowToast.setOnClickListener {
             // Code
             Log.i("MainActivity", "Button was clicked !")
-
-
             Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
-
         }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/msgshareapp/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.example.msgshareapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+
+
+
+
+}
